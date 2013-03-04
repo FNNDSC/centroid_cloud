80ed77c822128eb28bc51afb3dd9451494ac6e90
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2013-03-04 15:56:12 -0500

    Add normalization on cloud spaces to reduce rotational skew.

--- C_centroidCloud.py
+++ C_centroidCloud.py
@@ -26,6 +26,10 @@ class C_centroidCloud:
             'action'        : 'initializing base class, ',
             'error'         : 'it seems that no member keys are defined.',
             'exitCode'      : 10},
+        'noCloud'          : {
+            'action'        : 'initializing base class, ',
+            'error'         : 'it seems that no cloud data was provided.',
+            'exitCode'      : 11},
         'Save'              : {
             'action'        : 'attempting to pickle save self, ',
             'error'         : 'a PickleError occured',
@@ -48,7 +52,7 @@ class C_centroidCloud:
         """
             Simple "debug" print... based on verbosity level.
         """
-        if level <= self.m_verbosity: print str_txt
+        if level <= self.m_verbosity: print(str_txt)
 
     def verbosity_set( self, level ):
         self.m_verbosity = level
@@ -57,28 +61,28 @@ class C_centroidCloud:
                             astr_key,
                             ab_exitToOs=1
                             ):
-        print "FATAL ERROR"
-        print "\tSorry, some error seems to have occurred in <%s>::%s" \
-                        % ( self.__name__, self.__proc__ )
-        print "\tWhile %s" % C_centroidCloud.mdictErr[astr_key]['action']
-        print "\t%s" % C_centroidCloud.mdictErr[astr_key]['error']
-        print ""
+        print("FATAL ERROR")
+        print("\tSorry, some error seems to have occurred in <%s>::%s" \
+                        % ( self.__name__, self.__proc__ ))
+        print("\tWhile %s" % C_centroidCloud.mdictErr[astr_key]['action'])
+        print("\t%s" % C_centroidCloud.mdictErr[astr_key]['error'])
+        print("")
         if ab_exitToOs:
-            print "Returning to system with error code %d" % \
-                            C_centroidCloud.mdictErr[astr_key]['exitCode']
+            print("Returning to system with error code %d" % \
+                            C_centroidCloud.mdictErr[astr_key]['exitCode'])
             sys.exit( C_centroidCloud.mdictErr[astr_key]['exitCode'] )
         return C_centroidCloud.mdictErr[astr_key]['exitCode']
 
     def fatal( self, astr_key, astr_extraMsg="" ):
-        if len( astr_extraMsg ): print astr_extraMsg
+        if len( astr_extraMsg ): print(astr_extraMsg)
         self.error_exit( astr_key )
 
     def warn( self, astr_key, astr_extraMsg="" ):
         b_exitToOS = 0
-        if len( astr_extraMsg ): print astr_extraMsg
+        if len( astr_extraMsg ): print(astr_extraMsg)
         self.error_exit( astr_key, b_exitToOS )
 
-    def stats_calc(self, aM):
+    def stats_calc(self, aM, adict_stats):
         """
         Assumes that the cloud <aM> is in row order, i.e.
              x1    y1
@@ -87,13 +91,17 @@ class C_centroidCloud:
              xn     yn
         
         Dimensionality of space is equal to number of cols
+
+        Places stats (per column) in adict_stats.
         
-        Returns
+        Returns the stats dictionary.
         """
-        _dict_stats         = {}
-        _dict_stats['mean'] = np.mean(aM, 0)
-        _dict_stats['std']  = np.std(aM, 0)
-        return _dict_stats
+        adict_stats['mean'] = np.mean(aM, 0)
+        adict_stats['std']  = np.std(aM, 0)
+        adict_stats['min']  = np.min(aM, 0)
+        adict_stats['max']  = np.max(aM, 0)
+        adict_stats['range']= np.max(aM, 0) - np.min(aM,0)
+        return adict_stats
                 
     def rot_2D(self, aM, af_theta):
         """
@@ -135,6 +143,34 @@ class C_centroidCloud:
         M_Ctr       = aM.transpose()
         M_Crot      = np.dot(M_rot, M_Ctr)
         return M_Crot.transpose()
+
+    def cloudSpace_normalize(self, aM_cloud, o_adict_stats):
+        '''
+        This method "normalizes" the input cloud to a unit space along its
+        basis dimensions. This reduces rotational skew in the original space
+        which is particularly apparent when the original space deviations
+        beteen different dimensions are very large.
+
+        The normalized cloud is returned.
+        '''
+        self.stats_calc(aM_cloud, o_adict_stats)
+        _M              = (aM_cloud - o_adict_stats['min']) / \
+                            (o_adict_stats['max'] - o_adict_stats['min'])
+        return _M
+
+
+    def cloudSpace_denormalize(self, aM_cloud, adict_stats):
+        '''
+        This method "de-normalizes" the input cloud, based on
+        the values passed in adict_stats which holds the stats
+        of the original space.
+
+        Note, this method overwrites the passed arguments!
+        '''
+        _M = aM_cloud * \
+                    (adict_stats['max'] - adict_stats['min']) + \
+                    adict_stats['min']
+        return _M
     
     def projectionsOnAxes_find(self, adict_stats):
         """
@@ -153,7 +189,7 @@ class C_centroidCloud:
             v_ret[dim] = v_projection
         return v_ret
     
-    def projectionsOnAxes_rotate(self, av_projection, af_theta):
+    def projectionsOnAxes_rotate(self, av_projection, af_theta, a_d_stats):
         """
         Rotate projections in a reference axis frame by <af_theta>.
         
@@ -176,7 +212,7 @@ class C_centroidCloud:
             for endpoint in [0, 1]:
                 M_p[index, dim] = av_projection[dim][endpoint]
                 index += 1
-        M_center        = np.tile(self._dict_stats['mean'], 
+        M_center        = np.tile(a_d_stats['mean'],
                                   (2* self._M_Cdimensionality, 1))
         M_mask          = np.logical_not(M_p).astype(float)
         M_centerMask    = M_center * M_mask
@@ -195,7 +231,7 @@ class C_centroidCloud:
         v_extent    = np.zeros(self._M_Cdimensionality)
         for dim in np.arange(0, self._M_Cdimensionality):
             v_extent[dim] = av_projections[dim][1] - av_projections[dim][0]
-        return v_extent
+        return np.absolute(v_extent)
     
     def extent_init(self, v_extent):
         self._dict_extent2D['X']['min']     = v_extent[0] 
@@ -242,20 +278,27 @@ class C_centroidCloud:
         NOTE:
         * Only properly debugged for planar (i.e. 2D) boundaries.
         """
+        self.stats_calc(self._M_C, self._d_origCloudStats)
+        if self._b_normalizeCloudSpace:
+            self._M_C = self.cloudSpace_normalize(self._M_C, self._d_origCloudStats)
         for rotation in self._rotationKeys:
             f_angle = self._dict_rotationVal[rotation]
             f_rad   = np.deg2rad(f_angle)
             # First, rotation the cloud by -r_rad:
             neg_C   = self.rot_2D(self._M_C, -f_rad)
             # Now find the projections on the standard x/y axis:
-            self._dict_stats = self.stats_calc(neg_C)
-            v_projections = self.projectionsOnAxes_find(self._dict_stats)
+            self.stats_calc(neg_C, self._d_rotatedCloudStats[rotation])
+            self._d_rotatedCloudStats[rotation]['rotation'] = rotation
+            v_projections = self.projectionsOnAxes_find(self._d_rotatedCloudStats[rotation])
             v_extent = self.projectionExtent_find(v_projections)
+            if self._b_normalizeCloudSpace:
+                v_extent = self.cloudSpace_denormalize(v_extent, self._d_origCloudStats)
+                v_extent = np.absolute(v_extent)
             if self._b_debug:
-                print "%f %f %f %f %f" % (self._dict_rotationVal[rotation], 
+                print("%f %f %f %f %f" % (self._dict_rotationVal[rotation],
                                         v_extent[0], v_extent[1], 
                                         v_extent[0] + v_extent[1],
-                                        v_extent[0] * v_extent[1])
+                                        v_extent[0] * v_extent[1]))
             if rotation == 0: self.extent_init(v_extent)
             for key in self._dict_extent2D.keys():
                 if key == 'X':      self.extent_process('X', v_extent[0], 
@@ -268,7 +311,10 @@ class C_centroidCloud:
                 if key == 'XY':     self.extent_process('XY', 
                                         v_extent[0] * v_extent[1], 
                                         self._dict_rotationVal[rotation])
-            M_p = self.projectionsOnAxes_rotate(v_projections, f_rad)
+            M_p = self.projectionsOnAxes_rotate(v_projections, f_rad, \
+                                                self._d_rotatedCloudStats[rotation])
+            if self._b_normalizeCloudSpace:
+                M_p = self.cloudSpace_denormalize(M_p, self._d_origCloudStats)
             self._dict_Q[1][rotation] = M_p[1,:]
             self._dict_Q[2][rotation] = M_p[3,:]
             self._dict_Q[3][rotation] = M_p[0,:]
@@ -276,6 +322,10 @@ class C_centroidCloud:
         for quadrant in range(1, 5):
             for rotation in self._rotationKeys:
                 self._l_boundary.append(self._dict_Q[quadrant][rotation])
+        if self._b_normalizeCloudSpace:
+            self._M_C   = self.cloudSpace_denormalize(self._M_C, self._d_origCloudStats)
+        #self.minmaxComponent_analyze()
+
         
     def initialize(self):            
         """
@@ -294,6 +344,9 @@ class C_centroidCloud:
         self._rotationKeys      = range(0, self._numRotations)
         v_keys                  = np.array(self._rotationKeys)
         v_vals                  = v_keys * 90.0/self._numRotations
+
+        for rotation in self._rotationKeys:
+            self._d_rotatedCloudStats[rotation] = self._dict_stats.copy()
         self._dict_rotationVal  = misc.dict_init(self._rotationKeys, 
                                                  list(v_vals))
         self._dict_projection   = misc.dict_init(self._rotationKeys, 
@@ -336,6 +389,15 @@ class C_centroidCloud:
         else:
             return self._f_dev
 
+    def normalize(self, *args):
+        """
+        Get/set the normalization flag.
+        """
+        if len(args):
+            self._b_normalizeCloudSpace = args[0]
+        else:
+            return self._b_normalizeCloudSpace
+
     def debug(self, *args):    
         """
         Get/set the debugging flag.
@@ -358,34 +420,50 @@ class C_centroidCloud:
             return self._numRotations
        
     def __init__( self, *args, **kwargs ):
-        self.__name__       = 'C_centroidCloud'
-        self.__proc__       = "__init__"
-        self._b_debug       = False
+        self.__name__                   = 'C_centroidCloud'
+        self.__proc__                   = "__init__"
+        self._b_debug                   = False
         
-        self._v_centroid    = np.zeros( (1, 2) )
+        self._v_centroid                = np.zeros( (1, 2) )
 
-        self._M_Crows               = -1
-        self._M_Ccols               = -1
-        self._M_Cdimensionality     = 0
+        self._M_C                       = None
+        self._M_Crows                   = -1
+        self._M_Ccols                   = -1
+        self._M_Cdimensionality         = 0
         
-        self._dict_stats            = {}
-        self._dict_stats['mean']    = []
-        self._dict_stats['std']     = []
-        self._f_dev                 = 1.0
-        
-        self._str_file              = ''
-        self._numRotations          = 90
-        self._b_deg                 = True
+        self._dict_stats                = {
+                'mean':         [],
+                'std':          [],
+                'min':          [],
+                'max':          [],
+                'range':        [],
+                'rotation':     0.0
+        }
+        self._l_min                     = []
+        self._l_max                     = []
+        self._d_origCloudStats          = self._dict_stats.copy()
+        self._d_rotatedCloudStats       = {}
+        self._f_dev                     = 1.0
+        self._b_normalizeCloudSpace     = False
+
+        self._str_file                  = ''
+        self._numRotations              = 90
+        self._b_deg                     = True
         for key, value in kwargs.iteritems():
-            if key == 'file':       self._str_file      = value
+            if key == 'file':
+                self._str_file      = value
+                self._M_C           = np.genfromtxt(self._str_file)
+            if key == 'cloud':      self._M_C           = value
             if key == 'rotations':  self._numRotations  = value
             if key == 'stdWidth':   self._f_dev         = value
+            if key == 'normalize':  self._b_normalizeCloudSpace = value
 
-        self._M_C                   = np.genfromtxt(self._str_file)
+        if not self._M_C.any():
+            self.fatal('noCloud')
 
         # The rotations are set/controlled by _numRotations which defines
         # the number of rotations between 0 and 90 degrees. The rotation values
-        # are stored in _dict_rotationVals, indexed by _rotationKeys. In the
+        # are stored in _dict_rotationVal, indexed by _rotationKeys. In the
         # trivial (default) case, the rotation vals and keys are identical.
         self._rotationKeys          = []
         self._dict_rotationVal      = {}
