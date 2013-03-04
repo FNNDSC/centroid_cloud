08cb36c96013e196b31f9258178bd9b37afa0616
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2013-03-04 15:55:24 -0500

    Add additional command line functionality to control normalization and axis equal.

--- centroidCloud_run.py
+++ centroidCloud_run.py
@@ -36,12 +36,30 @@ Gstr_synopsis = """
     
         -m <cloudFile>
         The cloud file to read.
-        
+
         -r <rotations>
         The number of rotations, evenly spread out between 0...90 degrees.
-        
+
         -s <stdWidth>
         The width of the standard deviation cloud
+
+        -z <zorder>
+        Change the 'z' ordering. By default, the confidence boundary is drawn
+        "on top" of the point plots with a default zorder=3. By specifying
+        a different zorder (such as '-z 1'), the boundary can be drawn below
+        the point plots.
+
+        -n 
+        If specified, will turn OFF clould normalization.
+
+        Default is to always normalize. Without normalizing, 
+        rotational skew can occur. Normalization adds extra operations 
+        (and hence time) to theprojection calculations.
+
+        -a
+        If specified, will set axis ranges equal. In cases where deviation is
+        much more along one axis than another, this will result in a very
+        "thin" boundary polygon.
         
         -e
         If specified, print an extent report that gives for each rotation
@@ -51,13 +69,7 @@ Gstr_synopsis = """
         -d
         If specified, pass a debug flag to the C_centroidCloud class that
         triggers additional reporting.
-        
-        -z <zorder>
-        Change the 'z' ordering. By default, the confidence boundary is drawn
-        "on top" of the point plots with a default zorder=3. By specifying 
-        a different zorder (such as '-z 1'), the boundary can be drawn below 
-        the point plots.
-                
+                        
         -x or -h
         Print this help page.
         
@@ -67,6 +79,8 @@ Gstr_cloudMatrix    = 'cloud.txt'
 Gb_saveFig          = False
 Gb_debugCloud       = False
 Gb_extentReport     = False
+Gb_normalize        = True
+Gb_axisEqual        = False
 Gi_zorder           = 3
 G_numRotations      = 90
 G_f_stdWidth        = 0.5
@@ -82,7 +96,7 @@ def deviation_plot(al_points, str_fillColor = 'red', str_edgeColor = 'black'):
     return poly
 
 try:
-    opts, remargs   = getopt.getopt(sys.argv[1:], 'hxm:s:r:dez:')
+    opts, remargs   = getopt.getopt(sys.argv[1:], 'hxm:s:r:dez:na')
 except getopt.GetoptError:
     sys.exit(1)
 
@@ -98,6 +112,10 @@ for o, a in opts:
         G_numRotations          = int(a)
     if (o == '-z'):
         Gi_zorder               = int(a)
+    if (o == '-n'):
+        Gb_normalize            = False
+    if (o == '-a'):
+        Gb_axisEqual            = True
     if (o == '-d'):
         Gb_debugCloud           = True
     if (o == '-e'):
@@ -106,13 +124,15 @@ for o, a in opts:
 C_cloud         = C_centroidCloud(file='%s' % Gstr_cloudMatrix, 
                                   stdWidth  = G_f_stdWidth,
                                   rotations = G_numRotations)
+C_cloud.normalize(Gb_normalize)
 C_cloud.debug(Gb_debugCloud)
 C_cloud.confidenceBoundary_find()
 M_cloud         = C_cloud.cloud()
 l_polygonPoints = C_cloud.boundary()
 
 figure()
-axis('equal')
+if Gb_axisEqual:
+    axis('equal')
 grid() 
 
 p1,     = plot(M_cloud[:,0], M_cloud[:,1], color='#FF0000', marker='*', ls='None',
